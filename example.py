c46b28b0fdd8b53d505c6b91af6baea583c9454d
pcrete <poom.wettayakorn@gmail.com>
2020-02-25 14:56:39 +0700

    added example notebook

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from nvision import ObjectDetection
 
 model = ObjectDetection(api_key=os.getenv('API_KEY'))
 
-# base64 encoed string
+# base64 encoded string
 with open(os.path.join('docs', 'street.jpg'), 'rb') as file:
     image = file.read()
     image = base64.b64encode(image).decode('utf-8')
